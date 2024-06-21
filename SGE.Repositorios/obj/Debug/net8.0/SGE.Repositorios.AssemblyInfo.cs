7643361b52fe8b47cdb462a4d4e4f64942a37660
Giancardonee <giancardonee@gmail.com>
2024-06-21 05:43:00 -0300

    commit

--- SGE.Repositorios/obj/Debug/net8.0/SGE.Repositorios.AssemblyInfo.cs
+++ SGE.Repositorios/obj/Debug/net8.0/SGE.Repositorios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+486bfaaf79d98cf2b0c647141d0f4cf2afddd7bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b67000080ab3d623791a54f2dcab1a9949ae9df")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
